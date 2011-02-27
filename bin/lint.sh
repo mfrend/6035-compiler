268e62b2220a8c7d7c1ef2d429f26f7cb15bd16b
David Koh <dkoh44@gmail.com>
2011-02-27 16:23:39 -0500

    Make lint error non-fatal

--- bin/lint.sh
+++ bin/lint.sh
@@ -3,20 +3,14 @@
 base=`dirname $0`/..
 
 echo 'Checking for indent width of two spaces.'
-if find $base/src $base/tests/src -name "*.java" |
-    xargs $base/lib/indent_finder.py | grep -v -n -E ': space 2$'; then
-  exit 1
-fi
+find $base/src $base/tests/src -name "*.java" |
+    xargs $base/lib/indent_finder.py | grep -v -n -E ': space 2$'
 
 echo 'Detecting trailing whitespace.'
-if find $base/src $base/tests/src -name "*.java" |
-    xargs grep -n -E ' $'; then
-  exit 2
-fi
+find $base/src $base/tests/src -name "*.java" |
+    xargs grep -n -E ' $'
 
 echo 'Detecting tabs.'
-if find $base/src $base/tests/src -name "*.java" |
-    xargs grep -n -E '	'; then
-  exit 2
-fi
+find $base/src $base/tests/src -name "*.java" |
+    xargs grep -n -E '	'; 
 
