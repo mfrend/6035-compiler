1a00ee41ac0a6af5997a0430d6ba855f25e7c9ef
David Koh <dkoh44@gmail.com>
2011-03-11 13:52:33 -0500

    Added javadoc to ASTParentVisitor

--- src/edu/mit/compilers/le02/stgenerator/ASTParentVisitor.java
+++ src/edu/mit/compilers/le02/stgenerator/ASTParentVisitor.java
@@ -3,6 +3,15 @@ package edu.mit.compilers.le02.stgenerator;
 import edu.mit.compilers.le02.ast.ASTNode;
 import edu.mit.compilers.le02.ast.ASTNodeVisitor;
 
+/**
+ * The ASTParentVisitor ensures that all nodes have the correct parent links,
+ * after the AST is created.
+ * 
+ * This is done in a visitor rather than at AST creation time to avoid
+ * further complicating the AST generation code.
+ * @author dkoh
+ *
+ */
 public final class ASTParentVisitor extends ASTNodeVisitor<ASTNode> {
   private ASTNode parent = null;
 
