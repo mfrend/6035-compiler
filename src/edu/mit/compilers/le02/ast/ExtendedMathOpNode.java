6d801f6700555e9ee75eeba82e843af7d08a3be3
Elizabeth Fong <lizfong@mit.edu>
2011-04-03 11:15:19 +0800

    further refactor.

--- src/edu/mit/compilers/le02/ast/ExtendedMathOpNode.java
+++ src/edu/mit/compilers/le02/ast/ExtendedMathOpNode.java
@@ -19,165 +19,83 @@ public class ExtendedMathOpNode {
   private MathOpNode node;
 
   public ExtendedMathOpNode(ExpressionNode n, MathOp op) {
-    this.parentOp = op;
+    parentOp = op;
     left = new ArrayList<ExpressionNode>();
     right = new ArrayList<ExpressionNode>();
     if (n instanceof MathOpNode) {
-      this.node = (MathOpNode) n;
-      this.expand();
+      node = (MathOpNode)n;
+      expand();
     } else {
-      this.left.add(n);
+      left.add(n);
     }
   }
 
   public void expand() {
-    if (this.parentOp == null) { // Op is not defined by parent
-      if (node.getOp() == MathOp.ADD) {
-        this.parentOp = MathOp.ADD;
-        ExtendedMathOpNode n1 = new ExtendedMathOpNode(
-            this.node.left, MathOp.ADD);
-        ExtendedMathOpNode n2 = new ExtendedMathOpNode(
-            this.node.right, MathOp.ADD);
-
-        this.left.addAll(n1.left);
-        this.left.addAll(n2.left);
-
-        this.right.addAll(n1.right);
-        this.right.addAll(n2.right);
-
-      } else if (node.getOp() == MathOp.SUBTRACT) {
-        this.parentOp = MathOp.ADD;
-        ExtendedMathOpNode n1 = new ExtendedMathOpNode(
-            this.node.left, MathOp.ADD);
-        ExtendedMathOpNode n2 = new ExtendedMathOpNode(
-            this.node.right, MathOp.ADD);
-
-        this.left.addAll(n1.left);
-        this.left.addAll(n2.right);
-
-        this.right.addAll(n1.right);
-        this.right.addAll(n2.left);
-
-      } else if (node.getOp() == MathOp.MULTIPLY) {
-        this.parentOp = MathOp.MULTIPLY;
-        ExtendedMathOpNode n1 = new ExtendedMathOpNode(
-            this.node.left, MathOp.MULTIPLY);
-        ExtendedMathOpNode n2 = new ExtendedMathOpNode(
-            this.node.right, MathOp.MULTIPLY);
-
-        this.left.addAll(n1.left);
-        this.left.addAll(n2.left);
-
-        this.right.addAll(n1.right);
-        this.right.addAll(n2.right);
-
-      } else if (node.getOp() == MathOp.DIVIDE) {
-        this.parentOp = MathOp.MULTIPLY;
-        ExtendedMathOpNode n1 = new ExtendedMathOpNode(
-            this.node.left, MathOp.MULTIPLY);
-        ExtendedMathOpNode n2 = new ExtendedMathOpNode(
-            this.node.right, MathOp.MULTIPLY);
-
-        this.left.addAll(n1.left);
-        this.left.addAll(n2.right);
-
-        this.right.addAll(n1.right);
-        this.right.addAll(n2.left);
-      } else {
-        this.left.add(node);
-      }
-
-    } else { // Op is defined by parent
-      if (node.getOp() == MathOp.ADD && this.parentOp == MathOp.ADD) {
-        ExtendedMathOpNode n1 = new ExtendedMathOpNode(
-            this.node.left, MathOp.ADD);
-        ExtendedMathOpNode n2 = new ExtendedMathOpNode(
-            this.node.right, MathOp.ADD);
-
-        this.left.addAll(n1.left);
-        this.left.addAll(n2.left);
-
-        this.right.addAll(n1.right);
-        this.right.addAll(n2.right);
-      } else if (node.getOp() == MathOp.SUBTRACT &&
-                 this.parentOp == MathOp.ADD) {
-        ExtendedMathOpNode n1 = new ExtendedMathOpNode(
-            this.node.left, MathOp.ADD);
-        ExtendedMathOpNode n2 = new ExtendedMathOpNode(
-            this.node.right, MathOp.ADD);
-
-        this.left.addAll(n1.left);
-        this.left.addAll(n2.right);
-
-        this.right.addAll(n1.right);
-        this.right.addAll(n2.left);
-      } else if (node.getOp() == MathOp.MULTIPLY &&
-                 this.parentOp == MathOp.MULTIPLY) {
-
-        ExtendedMathOpNode n1 = new ExtendedMathOpNode(
-            this.node.left, MathOp.MULTIPLY);
-        ExtendedMathOpNode n2 = new ExtendedMathOpNode(
-            this.node.right, MathOp.MULTIPLY);
-
-        this.left.addAll(n1.left);
-        this.left.addAll(n2.left);
-
-        this.right.addAll(n1.right);
-        this.right.addAll(n2.right);
-
-      } else if (node.getOp() == MathOp.DIVIDE &&
-                 this.parentOp == MathOp.MULTIPLY) {
-
-        ExtendedMathOpNode n1 = new ExtendedMathOpNode(
-            this.node.left, MathOp.MULTIPLY);
-        ExtendedMathOpNode n2 = new ExtendedMathOpNode(
-            this.node.right, MathOp.MULTIPLY);
-
-        this.left.addAll(n1.left);
-        this.left.addAll(n2.right);
-
-        this.right.addAll(n1.right);
-        this.right.addAll(n2.left);
-
-      } else {
-        // Element is not a MathOpNode or its OP is not the same as its parents
-        this.left.add(node);
-      }
+    MathOp op = node.getOp();
+    MathOp canonical = null;
+    switch (op) {
+     case ADD:
+     case SUBTRACT:
+      canonical = MathOp.ADD;
+      break;
+     case MULTIPLY:
+     case DIVIDE:
+      canonical = MathOp.MULTIPLY;
+     default:
+      // Element is something irreducible like modulo.
+      left.add(node);
+      return;
     }
+
+    ExtendedMathOpNode n1 = null;
+    ExtendedMathOpNode n2 = null;
+
+    if (parentOp == null) { // Op is not defined by parent
+      parentOp = canonical;
+    } else if (parentOp != canonical) {
+      // Element's OP is not the same as its parents.
+      left.add(node);
+      return;
+    }
+
+    n1 = new ExtendedMathOpNode(node.left, canonical);
+    n2 = new ExtendedMathOpNode(node.right, canonical);
+
+    left.addAll(n1.left);
+    left.addAll((op == canonical) ? n2.left : n2.right);
+
+    right.addAll(n1.right);
+    right.addAll((op == canonical) ? n2.right : n2.left);
   }
 
   @Override
   public boolean equals(Object o) {
-    if (o instanceof ExtendedMathOpNode) {
-      ExtendedMathOpNode other = (ExtendedMathOpNode) o;
-      if (this.parentOp == other.parentOp) {
-        // Check lhs
-        List<ExpressionNode> l = other.left;
-        for (ExpressionNode t : this.left) {
-          if (!(l.contains(t))) {
-            return false;
-          } else {
-            l.remove(t);
-          }
-        }
-        if (l.size() != 0) {
-          return false;
-        }
-        // Check rhs
-        List<ExpressionNode> r = other.right;
-        for (ExpressionNode t : this.right) {
-          if (!(r.contains(t))) {
-            return false;
-          } else {
-            r.remove(t);
-          }
-        }
-        if (r.size() != 0) {
-          return false;
-        }
-        return true;
+    if (!(o instanceof ExtendedMathOpNode)) {
+      return false;
+    }
+    ExtendedMathOpNode other = (ExtendedMathOpNode) o;
+    if (this.parentOp != other.parentOp) {
+      return false;
+    }
+    // Check lhs
+    return unorderedEquals(new ArrayList<ExpressionNode>(other.left),
+                           new ArrayList<ExpressionNode>(this.left)) &&
+           unorderedEquals(new ArrayList<ExpressionNode>(other.right),
+                           new ArrayList<ExpressionNode>(this.right));
+  }
+
+  private boolean unorderedEquals(List<ExpressionNode> theirs,
+                                  List<ExpressionNode> mine) {
+    for (ExpressionNode t : mine) {
+      if (!(theirs.contains(t))) {
+        return false;
+      } else {
+        theirs.remove(t);
       }
     }
-    return false;
+    if (theirs.size() != 0) {
+      return false;
+    }
+    return true;
   }
 }
