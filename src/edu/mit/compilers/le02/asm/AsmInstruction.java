8cc4a72654159dfc1213cd39ff03a06d49d17cec
Maria Frendberg <mfrend@mit.edu>
2011-04-22 14:55:36 -0400

    More Asm Object fixes

--- src/edu/mit/compilers/le02/asm/AsmInstruction.java
+++ src/edu/mit/compilers/le02/asm/AsmInstruction.java
@@ -19,23 +19,42 @@ public class AsmInstruction implements AsmObject {
 
 	AsmOpCode opcode;
 	SourceLocation loc;
-	Object first_operand, second_operand;
-	
-	//TODO: Fix case enumeration (mfrend)
+	String first_operand, second_operand;
 
 	public AsmInstruction(AsmOpCode opCode, SourceLocation loc) {
-		this(opCode,null,null,loc);
+		this(opCode, "", "", loc);
 	}
 
-	public AsmInstruction(AsmOpCode opCode, Object first_operand,
+	public AsmInstruction(AsmOpCode opCode, String first_operand,
 			SourceLocation loc) {
-		this(opCode,first_operand,null,loc);
+		this(opCode, first_operand, "", loc);
 	}
 
-	public AsmInstruction(AsmOpCode opCode, Object first_operand,
-			Object second_operand, SourceLocation loc) {
-		assert (first_operand instanceof String || first_operand instanceof Register);
-		assert (second_operand instanceof String || first_operand instanceof Register);
+	public AsmInstruction(AsmOpCode opCode, Register first_operand,
+			SourceLocation loc) {
+		this(opCode, first_operand.toString(), "", loc);
+	}
+
+	public AsmInstruction(AsmOpCode opCode, Register first_operand,
+			Register second_operand, SourceLocation loc) {
+
+		this(opCode, first_operand.toString(), second_operand.toString(), loc);
+	}
+
+	public AsmInstruction(AsmOpCode opCode, Register first_operand,
+			String second_operand, SourceLocation loc) {
+		
+		this(opCode, first_operand.toString(), second_operand, loc);
+	}
+
+	public AsmInstruction(AsmOpCode opCode, String first_operand,
+			Register second_operand, SourceLocation loc) {
+
+		this(opCode, first_operand, second_operand.toString(), loc);
+	}
+
+	public AsmInstruction(AsmOpCode opCode, String first_operand,
+			String second_operand, SourceLocation loc) {
 
 		this.opcode = opCode;
 		this.first_operand = first_operand;
@@ -44,14 +63,14 @@ public class AsmInstruction implements AsmObject {
 	}
 
 	public String toString() {
-		if (this.first_operand == null) {
-			return "  " + opcode.toString() + getOriginalSource(loc);
-		} else if (this.second_operand == null) {
-			return "  " + opcode.toString() + " " + first_operand + getOriginalSource(loc);
-		} else {
-			return "  " + opcode.toString() + " " + first_operand + ", " + second_operand + 
-				getOriginalSource(loc);
+		String result = "  " + opcode.toString();
+		if (this.first_operand != "") {
+			result += " " + first_operand;
+			if (this.second_operand != "") {
+				result += ", " + second_operand;
+			}
 		}
+		return result + getOriginalSource(loc);
 	}
 
 	/**
